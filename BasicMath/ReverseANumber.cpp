14f314e486b8477d2196c058f35c60fe3e160c16
Alapan000 <alapandas2810@gmail.com>
2024-11-04 01:05:07 +0530

    Seventh Commit

--- BasicMath/ReverseANumber.cpp
+++ BasicMath/ReverseANumber.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 //4567---->7654
 //5300---->35
-
 int reverseNumber(int Num){
+    int is_neg = Num > 0 ? 1 : -1;
+    Num *= is_neg;
     int reverse_Number = 0; 
     while(Num>0){
         int last_digit = Num % 10;
@@ -13,12 +14,12 @@ int reverseNumber(int Num){
         if (reverse_Number > INT_MAX / 10 || (reverse_Number == INT_MAX / 10 && last_digit > 7)) {
                 return 0;  // Overflow
             }
-            if (reverse_Number < INT_MIN / 10 || (reverse_Number == INT_MIN / 10 && last_digit < -8)) {
-                return 0;  // Underflow
-            }
+            // if (reverse_Number < INT_MIN / 10 || (reverse_Number == INT_MIN / 10 && last_digit < -8)) {
+            //     return 0;  // Underflow
+            // }
         reverse_Number = (reverse_Number*10) + last_digit;
     }
-    return reverse_Number;
+    return reverse_Number*is_neg;
 }
 int main(){
     int number;
