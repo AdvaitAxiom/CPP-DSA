90239206f02dfeab53b9d261e53d13fbee7bddce
Alapan000 <alapandas2810@gmail.com>
2024-11-05 17:16:53 +0530

    Divisors of a Number with sqrt

--- BasicMath/AllDivisorsBetter.cpp
+++ BasicMath/AllDivisorsBetter.cpp
@@ -1,14 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-//T(C) = O(number)
+
 void divisors(int number){
     vector<int> div;
-
-    for(int i=1; i<=number; i++){
+    //O(sqrt(number))
+    for(int i=1; i<=sqrt(number); i++){
         if(number % i == 0 ){
             div.push_back(i);
+            if(number/i != i){
+             div.push_back(number/i);
+            }
         }
     }
+    //O(no of factors *log (no. of factors ))
+    sort(div.begin(), div.end());
+
+    //O(number of factors)
     for(const auto &p: div){
         cout<< p <<" ";
     }
