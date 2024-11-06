3822d9632a8617fefd10d43028122ebb009a9bd8
Alapan000 <alapandas2810@gmail.com>
2024-11-06 14:43:09 +0530

    Euclidean Algo

---
+++ BasicMath/Euclidean Algo.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//gcd(n1,n2) = gcd(n1-n2,n2) WHERE n1>n2
+
+/*
+gcd(20,15)->5
+=gcd(5,15)->5 
+=gcd(15,5)->5
+=gcd(10,5)->5
+=gcd(5,5)->5
+=gcd(0,5) STOP(N1==0), [ANS=N2]
+
+
+*/
+
+int main(){
+
+    return 0;
+}
