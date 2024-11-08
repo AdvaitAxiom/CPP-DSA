e11e6c512100b75b3c90415975a63d182e3a92ee
Alapan000 <alapandas2810@gmail.com>
2024-11-09 03:10:50 +0530

    Print GFG_nTimes Updated

--- Recursion/PrintGFG_nTimes.cpp
+++ Recursion/PrintGFG_nTimes.cpp
@@ -10,11 +10,30 @@ void printGFG(int n){
     counting++;
     printGFG(n);
 }
+//TC->O(n)
+//Space complexity is the stack space. O(n)
+//Without using Global Variable
+void f(int i, int n){
+    if(i<=0 || i>n)return;
+    cout<<"Alapan ";
+    f(i+1,n);
+}
+
+//Passing 1 argument
+void f2(int n){
+    if(n<=0)return;
+    cout<<"IT ";
+    f2(n-1);
+}
 
 int main(){
     int n;
     cout<<"Number of iterations: ";
     cin>>n;
     printGFG(n);
+    cout<<endl;
+    f(1,n);
+    cout<<endl;
+    f2(n);
     return 0;
 }
