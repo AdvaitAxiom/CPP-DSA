d4c056ae70dbf69e4bd5fa9a3763c2362cfc7728
Alapan000 <alapandas2810@gmail.com>
2024-11-10 20:55:04 +0530

    ReverseArray

---
+++ Recursion/ReveseArray.cpp
@@ -0,0 +1,32 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+
+void swaparr(int i,int arr[],int size){
+    if(i>=size/2)return;
+    swap(arr[i],arr[size-i-1]);
+    swaparr(i+1,arr,size);
+}
+ 
+ 
+ int main(){
+//Size
+    int size;
+    cout<<"Enter Size: ";
+    cin >> size;
+//Array
+    int arr[size];
+//Array Element Input
+    cout<<"Enter the numbers: ";
+
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+//Swap
+    swaparr(0,arr,size);
+//Array Element Output
+    cout<<"Your array: "<<endl;
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+ }
