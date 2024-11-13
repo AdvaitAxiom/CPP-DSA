50a1e5496b4154c7033c9755d914a73454a72226
Alapan000 <alapandas2810@gmail.com>
2024-11-14 01:24:49 +0530

    rValue_lValue_references_example

---
+++ Move Semantics/rValue_lValue_references_example.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void f(int& i){cout<<"L-Value f"<<endl;}
+void f(int&& i){cout<<"R-Value f"<<endl;}
+int main(){
+    int i =10; //i->lvalue & 10 r-value
+
+    f(i);   //L-Value f
+    f(10);  //R-Value f
+    f(move(i)); //R-Value f
+    //It takes an l-value and convrts it to r-value
+    return 0;
+}
