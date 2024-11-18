7a811591f670cc5bdd28dae5c17671cca5d56d87
Alapan000 <alapandas2810@gmail.com>
2024-11-18 16:15:58 +0530

    Character Hashing Generic

---
+++ Hashing/charHashingGeneric.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str;
+    cout<< "Enter the string: "<<endl;
+    cin >> str;
+
+    int qu;
+    cout<< "Enter the number of queries: "<<endl;
+    cin >> qu;
+
+    int hash[256] = {0};
+
+    for(int i = 0; i<str.size(); i++){
+        hash[str[i]]++;
+    }
+
+    while(qu--){
+        cout<< "Enter the character to search: "<<endl;
+        char c;
+        cin >> c;
+        cout<< "The frequency is: "<<hash[c]<<endl;
+
+
+    }
+    return 0;
+}
