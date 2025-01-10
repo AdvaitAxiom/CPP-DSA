ba76132c1e1666d18499d1191931ee6397f05be8
Alapan000 <alapandas2810@gmail.com>
2025-01-10 15:39:55 +0530

    Updated with Solution

--- Hashing/FrequenciesofLimitedRangeArrayElements.cpp
+++ Hashing/FrequenciesofLimitedRangeArrayElements.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 //You are given an array arr[] containing positive integers. The elements in the array arr[] range from 1 to n (where n is the size of the array), and some numbers may be repeated or absent. Your task is to count the frequency of all numbers in the range 1 to n and return an array of size n such that result[i] represents the frequency of the number i (1-based indexing).
 
+//Solution----->>https://www.geeksforgeeks.org/problems/frequency-of-array-elements-1587115620/0
+
 // } Driver Code Ends
 class Solution {
   public:
