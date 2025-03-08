82f194338e286471fe88298a74e313b1bbc868f5
Alapan000 <alapandas2810@gmail.com>
2025-03-08 20:05:43 +0530

    Printing the count of Subsequence

---
+++ Recursion/CertainNumberOfSubsequences.cpp
@@ -0,0 +1,28 @@
+// PRINTING SUBSEQUENCES WHOSE SUM IS K
+#include<bits/stdc++.h>
+using namespace std;
+
+//RIGHT WAY
+int printSS(int ind, int s, int sum,int arr[], int n){
+    if(ind ==n){
+        if(s == sum ){
+            return 1;
+        }
+        else return 0;
+    }
+    //pick
+    s += arr[ind];
+    int l =printSS(ind+1,s, sum,arr, n);
+    s-=arr[ind];
+
+    //not pick
+    int r=printSS(ind+1,s,sum,arr, n) == true;
+    return l+r;
+}
+int main(){
+    int arr[] = {1,2,1};
+    int n =3;
+    int sum = 2;
+    cout<< printSS(0,0,sum,arr,n);
+    return 0;
+}
