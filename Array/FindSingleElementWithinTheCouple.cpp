04c5374d88ea1f94bdaa805d15dc3221d8bab193
Alapan000 <alapandas2810@gmail.com>
2025-05-01 12:40:20 +0530

    Find the number that appears once, and the other numbers twice

---
+++ Array/FindSingleElementWithinTheCouple.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getSingleElement(vector <int> nums){
+    int xor1 = 0;
+
+    for(int i = 0; i<nums.size();i++){
+        xor1 ^= nums[i];  
+    }
+    return xor1;
+}
+
+int main(){
+    vector<int> arr = {4, 1, 2, 1, 2};
+    int ans = getSingleElement(arr);
+    cout << "The single element is: " << ans << endl;
+    return 0;
+}
